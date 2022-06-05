b27d28139d19f328f2eac43b31e62a34165fbbb2
chenwingsing <742474834@qq.com>
2022-06-05 16:08:26 +0800

    Delete community-develop directory

--- community-develop/src/main/java/com/nowcoder/community/dao/elasticsearch/DiscussPostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nowcoder.community.dao.elasticsearch;
-
-import com.nowcoder.community.entity.DiscussPost;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository//注意不是mapper，那是mybatis专用的，repository是srping提供的
-public interface DiscussPostRepository extends ElasticsearchRepository<DiscussPost, Integer> {
-}
