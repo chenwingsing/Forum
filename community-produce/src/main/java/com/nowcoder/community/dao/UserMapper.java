4549128e5222a74dc24c8fdf4aa84d117d313441
chenwingsing <742474834@qq.com>
2022-06-07 08:26:28 +0800

    Add files via upload

--- community-produce/src/main/java/com/nowcoder/community/dao/UserMapper.java
+++ community-produce/src/main/java/com/nowcoder/community/dao/UserMapper.java
@@ -3,6 +3,7 @@ package com.nowcoder.community.dao;
 
 import com.nowcoder.community.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Mapper //mybatis的注解，当然了你也可以写Repository（视频好像是这么说的，但是我试了会报错）
@@ -12,9 +13,9 @@ public interface UserMapper {
     User selectByEmail(String email);
 
     int insertUser(User user);//注意这个有点不一样，这个是一个对象，所以在mapper的xml在insert语句要声明
-    int updateStatus(int id, int status);
-    int updateHeader(int id, String headerUrl);
-    int updatePassword(int id, String password);
+    int updateStatus(@Param("id") int id, @Param("status") int status);
+    int updateHeader(@Param("id") int id, @Param("headerUrl") String headerUrl);
+    int updatePassword(@Param("id") int id, @Param("password") String password);
 
 
 
